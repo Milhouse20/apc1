15d13a2b0447afb00e119cc51c6c9b8489d4aa27
João Enrique Oliveira Souza <160737795+Milhouse20@users.noreply.github.com>
2024-03-06 13:52:18 +0000

    codigos da aula02 e da pratica1

---
+++ aulas/aula02/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+int numero;
+  char caracter1;
+  int a; // essa variavel eh ler um numero do coeficiente
+  int x;
+float nome_composto;
+
+  numero = 10;
+
+  numero = 100;
+  
+  return 0;
+}
